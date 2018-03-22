e2fd2415276e50665a41447d1a8b05559ff5b6a9
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 14:51:54 +0900

    Add files via upload

---
+++ Clsandobj.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Clsandobj {
+
+	public static void main(String[] args) {
+		Message msg = new Message();
+		msg.HelloWorld();
+
+	}
+
+}
+
+class Message {
+
+	public void HelloWorld() {
+		System.out.println("Hello World");
+
+	}
+
+}
