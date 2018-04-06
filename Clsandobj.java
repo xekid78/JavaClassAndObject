695fd75e447ef5ba690aacbd98acfd1b32708bb8
Makoto Tanabe <Xekid@outlook.com>
2018-04-06 14:53:13 +0900

    main()メソッドなどを一番下に移動させる

--- Clsandobj.java
+++ Clsandobj.java
@@ -3,21 +3,15 @@
  * @author xekid78
  *
  */
-public class Clsandobj {
+ class Message {
+ 	public void HelloWorld() {
+ 		System.out.println("Hello World");
+ 	}
+ }
 
+public class Clsandobj {
 	public static void main(String[] args) {
 		Message msg = new Message();
 		msg.HelloWorld();
-
 	}
-
-}
-
-class Message {
-
-	public void HelloWorld() {
-		System.out.println("Hello World");
-
-	}
-
 }
